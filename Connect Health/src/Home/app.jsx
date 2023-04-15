94a778ff00a786dc3a360879b803adf9946c885d
willianrizzo <willianrizzoratao10@gmail.com>
2023-04-14 21:12:56 -0300

    teste de tailwind

---
+++ Connect Health/src/Home/app.jsx
@@ -0,0 +1,8 @@
+function Home() {
+    return(
+        <div className="bg-red-600 flex justify-center">
+            <h1 className="text-center text-[#fff]">Penis</h1>
+        </div>
+    )
+}
+export default Home
