afc6df96d14375639a6364c000d89e17c381f931
wendel <wh68639@gmail.com>
2023-04-22 15:55:29 -0300

    filtros OK

---
+++ Connect Health/src/Nutricao/Components/Filtros.jsx
@@ -0,0 +1,102 @@
+import React from 'react'
+import Radio from "@mui/material/Radio"
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControl from '@mui/material/FormControl';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Menu from "@mui/material/Menu"
+import MenuItem from "@mui/material/MenuItem"
+import { useState } from 'react';
+import Backdrop from '@mui/material/Backdrop';
+import CircularProgress from '@mui/material/CircularProgress';
+
+import { MdTune } from "react-icons/md"
+import { AiOutlineArrowDown } from "react-icons/ai"
+
+
+const Filtros = () => {
+    const [open, setOpen] = useState(null)
+
+    const close = () => {
+        setOpen(null)
+    }
+
+    const handleClick = (event) => {
+        setOpen(event.currentTarget)
+    }
+
+    const [backdrop, setBackdrop] = useState(false)
+    const handleClose = () => {
+        setBackdrop(false);
+      };
+    const handleOpen = () => {
+        setBackdrop(true);
+    };
+
+  return (
+    <div className='mt-20'>
+        <h2 className='text-center text-nutri font-bold text-3xl mb-14'>
+            Nossos Profissionais
+        </h2>
+
+        <div  className='flex justify-between mx-14'>
+            <div className='flex items-center'>
+                <FormControl>
+                <RadioGroup
+                    row
+                    aria-labelledby="demo-row-radio-buttons-group-label"
+                    name="row-radio-buttons-group"
+                >
+                     <FormControlLabel  value="todos" control={<Radio />} label="Todos"  />
+                    <FormControlLabel value="recomendados" control={<Radio />} label="Recomendados" />
+                </RadioGroup>
+                </FormControl>
+
+                
+            </div>
+
+            <div className='flex gap-10'>
+                <button onClick={handleOpen} className='flex items-center font-semibold gap-1'>
+                    Filtros
+                    <MdTune />
+                </button>
+
+                <Backdrop
+                    sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+                    open={backdrop}
+                    onClick={handleClose}
+                >
+                    <CircularProgress color="inherit" />
+                </Backdrop>
+
+                <button onClick={(e) => handleClick(e)} className='flex items-center font-semibold border py-0.5 px-3 rounded-3xl gap-1'>
+                    Relevância
+                    <AiOutlineArrowDown />
+
+                </button>
+
+                <Menu
+                    anchorEl={open}
+                    open={open}
+                    onClose={close}
+                >
+                    <MenuItem onClick={() => {
+                        close()
+                    }}>Melhores avaliados</MenuItem>
+                    <MenuItem onClick={() => {
+                        close()
+                    }}>Menor Preço</MenuItem>
+                    <MenuItem onClick={() => {
+                        close()
+                    }}>Maior Preço</MenuItem>
+                </Menu>
+
+                
+            </div>
+        </div>
+
+
+    </div>
+  )
+}
+
+export default Filtros
--- Connect Health/src/Nutricao/Nutricao.jsx
+++ Connect Health/src/Nutricao/Nutricao.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 
 import HomeNutri from './Components/HomeNutri'
 import Detalhes from './Components/Detalhes'
+import Filtros from './Components/Filtros'
 
 const Nutricao = () => {
   return (
     <div>
       <HomeNutri />
       <Detalhes />
+      <Filtros />
+
+      
     </div>
   )
 }
