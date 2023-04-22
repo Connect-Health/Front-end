63a0ef0a59dff026541ac10e690478b1a37be6dd
wendel <wh68639@gmail.com>
2023-04-22 17:38:52 -0300

    animated Letters

--- Connect Health/src/Nutricao/Components/HeaderNutri.jsx
+++ Connect Health/src/Nutricao/Components/HeaderNutri.jsx
@@ -3,17 +3,17 @@ import React from 'react'
 const Header = () => {
   return (
     <div className='flex justify-between items-center py-3 px-10'>
-        <h1 className='font-semibold text-white text-lg'>
-            <span className='text-nutri '>Connect</span>
+        <h1 className='font-semibold text-white text-lg hover:text-nutri w-6 transition duration-300 hover:scale-110'>
+            <span className='text-nutri hover:text-white w-6 transition duration-300 hover:scale-110 '>Connect</span>
             Health
         </h1>
 
-        <h2 className='text-white font-bold uppercase text-base ml-28'>Nutrição</h2>
+        <h2 className='text-white font-bold uppercase text-base ml-28 hover:scale-110 hover:text-[#000] transition duration-300'>Nutrição</h2>
 
-        <div className='flex gap-3 '>
-            <h2 className='text-white uppercase text-sm'>Psicologia</h2>
-            <h2 className='text-white uppercase text-sm'>Feed de Noticias</h2>
-            <h2 className='text-white uppercase text-sm'>Cadastro</h2>
+        <div className='flex gap-3 z-10 '>
+            <h2 className='text-white uppercase text-sm hover:scale-110 hover:font-semibold transition duration-300'>Psicologia</h2>
+            <h2 className='text-white uppercase text-sm hover:scale-110 hover:font-semibold transition duration-300'>Feed de Noticias</h2>
+            <h2 className='text-white uppercase text-sm hover:scale-110 hover:font-semibold transition duration-300'>Cadastro</h2>
 
         </div>
     </div>
--- Connect Health/src/Nutricao/Components/HomeNutri.jsx
+++ Connect Health/src/Nutricao/Components/HomeNutri.jsx
@@ -2,14 +2,115 @@ import React from 'react'
 import Header from './HeaderNutri'
 import { MdSubdirectoryArrowRight } from "react-icons/md"
 
+import { useEffect, useState } from 'react';
+import AnimatedLetters from '../../Components/Letters/AnimatedLetters'
+
+
 const HomeNutri = () => {
+    const [letterClass, setLetterClass] = useState('text-animate');
+    useEffect(() => {
+        setTimeout(() => {
+        return  setLetterClass('text-animate-hover')
+        }, 3000)
+    }, [])
+
   return (
     <div className='bg-nutriImg h-[650px] flex flex-col'>
         <Header />
         <div className='h-[655px] w-[430px] mt-[-55px] bg-white/40 relative self-end flex justify-center items-center flex-col'>
 
-          <h2 className='w-[80%] font-bold text-3xl text-center mt-10'>
-            A nutrição é a base da saúde.<br /> Coma bem, sinta-se bem e viva bem!
+          <h2 className='w-[85%] font-bold text-3xl text-center mt-10'>
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={[
+              'A', 
+              ' ', 
+              'n', 
+              'u', 
+              't', 
+              'r', 
+              'i', 
+              'ç', 
+              'ã', 
+              'o', 
+              ' ', 
+              'é', 
+              ' ', 
+              'a', 
+              ' ', 
+              'b',
+              'a', 
+              's', 
+              'e', 
+              ' ', 
+              'd', 
+              'a',
+            ]}
+            idx={21} 
+          /> 
+          
+          <br />
+
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={[
+              'S',
+              'a',
+              'ú',
+              'd',
+              'e',
+            ]}
+            idx={5}
+          /> <br />
+
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={[
+              'C',
+              'o',
+              'm',
+              'a',
+              ' ',
+              'b',
+              'e',
+              'm',
+              ',',
+              ' ',
+              's',
+              'i',
+              'n',
+              't',
+              'a',
+              '-',
+              's',
+              'e',
+            ]}
+            idx={18}
+          /> <br />
+
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={[
+              'b',
+              'e',
+              'm',
+              ' ',
+              'e',
+              ' ',
+              'v',
+              'i',
+              'v',
+              'a',
+              ' ',
+              'b',
+              'e',
+              'm',
+              '!',
+            ]}
+            idx={18}
+          />
+
+          
           </h2>
 
           <p className='w-[90%] text-center font-semibold mt-16 mb-6 text-lg '>
