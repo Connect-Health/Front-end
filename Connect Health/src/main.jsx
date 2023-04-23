7727dd68b9d566a3a81adb026f35d27989fef36e
wendel <wh68639@gmail.com>
2023-04-22 22:54:06 -0300

    teste

--- Connect Health/src/main.jsx
+++ Connect Health/src/main.jsx
@@ -21,7 +21,8 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <RouterProvider router={router}
+    fallbackElement='/' />
     
   </React.StrictMode>,
 )
