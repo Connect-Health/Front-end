40a67bf031ff3f1e9302159f35a903ff6f763baa
wendel <wh68639@gmail.com>
2023-04-27 21:29:32 -0300

    function onclick

--- Connect Health/src/Nutricao/Components/Click.jsx
+++ Connect Health/src/Nutricao/Components/Click.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Click = (props) => {
+
+  
+  
+  const handleClick = (e) => {
+    if(e.target.classList.contains('bg-nutri') === false) {
+      e.target.classList.add('bg-nutri')
+      e.target.classList.add('text-white')
+
+    } else {
+      e.target.classList.remove('bg-nutri')
+      e.target.classList.remove('text-[#000]')
+
+    }
+  }
+
+
   return (
     <div>
-        <p className='border border-black text-black font-semibold cursor-pointer rounded-3xl px-3 text-sm hover:scale-110 transition-all duration-100 active:scale-90'>{props.text}</p>
+        <p onClick={handleClick} className='border border-black text-black font-semibold cursor-pointer rounded-3xl px-3 text-sm hover:scale-110 transition-all duration-100 active:scale-90 checked:bg-nutri'>{props.text}</p>
     </div>
   )
 }
