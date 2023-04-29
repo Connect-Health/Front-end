f7b0ef588105cbada2f3a83b6f3cbea314479083
wendel <wh68639@gmail.com>
2023-04-29 13:49:25 -0300

    ok filtros

--- Connect Health/src/Psicologia/Components/Click.jsx
+++ Connect Health/src/Psicologia/Components/Click.jsx
@@ -1,27 +1,25 @@
-import React from 'react'
-
-const Horario = (props) => {
+const Click = (props) => {
 
   const handleClick = (e) => {
     if(e.target.classList.contains('bg-psi') === false) {
       e.target.classList.add('bg-psi')
       e.target.classList.add('text-white')
       e.target.classList.add('font-light')
-      e.target.classList.add('border-[#fff]')
+
 
     } else {
       e.target.classList.remove('bg-psi')
       e.target.classList.remove('font-light')
       e.target.classList.remove('text-white')
-      e.target.classList.remove('border-[#fff]')
     }
   }
 
+
   return (
     <div>
-        <p onClick={handleClick} className='border border-black text-black w-16 text-center cursor-pointer rounded-3xl font-semibold text-sm hover:scale-125 duration-200 transition-all active:scale-100'>{props.hora}</p>
+        <p onClick={handleClick} className='border border-black text-black font-semibold cursor-pointer rounded-3xl px-3 text-sm hover:scale-110 transition-all duration-100 active:scale-90'>{props.text}</p>
     </div>
   )
 }
 
-export default Horario
+export default Click;
