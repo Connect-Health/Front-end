ecb5c33e9d8fac0028d35041f65c9aebbfbe9b93
willrizzo <willianrizzoratao10@gmail.com>
2023-04-30 19:24:12 -0300

    Inicio Psicologia

---
+++ Connect Health/src/Home/Componentes/Psico.jsx
@@ -0,0 +1,13 @@
+import Wave from '../../../assets/purplewave.png'
+
+function Psico(){
+    return(
+        <div >
+           <img src={Wave} alt="" />
+            <div>
+                
+            </div>
+        </div>
+    )
+}
+export default Psico
