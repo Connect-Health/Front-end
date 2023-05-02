15fe3c659c40559b3b42e0400c147c21a7418070
wendel <wh68639@gmail.com>
2023-05-02 12:36:30 -0300

    card ok

--- Connect Health/src/Nutricao/Components/Card.jsx
+++ Connect Health/src/Nutricao/Components/Card.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Card = ({ profissional }) => {
-  const MAX_ESPECIALIDADES = 4;
+  const MAX_ESPECIALIDADES = 3;
   const especiaisRestantes = profissional.especialidade.length - MAX_ESPECIALIDADES;
   
   return (
@@ -21,7 +21,7 @@ const Card = ({ profissional }) => {
       </div>
 
       <div className='w-[75%] '>
-        <div className='w-[90%] m-auto flex flex-col'>
+        <div className='w-[90%] h-full m-auto flex flex-col justify-between items-center'>
           <h2 className='mt-4 font-semibold text-2xl text-center text-nutri'>{profissional.nome} <span>
               {profissional.sobrenome}</span>
           </h2>
@@ -32,10 +32,10 @@ const Card = ({ profissional }) => {
                 {profissional.especialidade.slice(0, MAX_ESPECIALIDADES).map((especial) => (
                   <p key={especial} className='border w-fit rounded-lg px-2 bg-white'>{especial}</p>
                 ))}
-                <span className='border w-fit rounded-lg px-2 bg-white'>{`+${especiaisRestantes}`}</span>
+                <span className='border w-fit rounded-lg px-2 self-end bg-white'>{`+${especiaisRestantes}`}</span>
               </div>
             ) : (
-              <div className='flex flex-wrap bg-[#D7F2E0] py-3 rounded-xl px-2 gap-2 justify-evenly'>
+              <div className='flex flex-wrap bg-[#D7F2E0] py-3 rounded-xl px-2 gap-2'>
                 {profissional.especialidade.map((especial) => (
                   <div>
                     <span className='border w-fit rounded-lg px-2 bg-white py-1' key={especial}>{especial}</span>
@@ -50,7 +50,8 @@ const Card = ({ profissional }) => {
             <p className=''>Olá! Meu nome é Laysa e sou uma nutricionista apaixonada pelo meu trabalho. Eu me formei em uma universidade renomada e tenho experiência em diferentes {profissional.descricao}</p>
           </div>
 
-          <button className='self-end justify-self-end px-3 mr-[-6%] py-1.5 rounded-2xl bg-nutri bg-opacity-80 font-medium text-white'>Saiba mais</button>
+            <button className='self-end px-3 mr-[-6%] py-1.5 rounded-2xl bg-nutri bg-opacity-80 font-medium text-white'>Saiba mais</button>
+
         </div>
 
       </div>
