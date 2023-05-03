6ac607e4e1c09ff61bf313e95bfc2c8572bb4bb4
wendel <wh68639@gmail.com>
2023-05-03 19:30:04 -0300

    ok

--- Connect Health/src/Nutricao/Components/Card.jsx
+++ Connect Health/src/Nutricao/Components/Card.jsx
@@ -1,23 +1,31 @@
 import React from 'react';
+import Rating from '@mui/material/Rating';
 
 const Card = ({ profissional }) => {
   const MAX_ESPECIALIDADES = 3;
   const especiaisRestantes = profissional.especialidade.length - MAX_ESPECIALIDADES;
-  
+
+  const descricaoProfissional = profissional.descricao
+
   return (
     <div key={profissional.id} className='flex rounded-2xl border-2 bg-[#F5F5F5] bg-opacity-50 border-[#94E127] box-content'>
-      <div className='w-[25%]'>
+      <div className='w-[25%] border-r border-nutri flex flex-col'>
         <img src={profissional.avatar} className='m-auto mt-3 rounded-xl w-24 h-auto' />
 
         <div className='mt-5'>
           <h4 className='text-center font-semibold'>Duração:</h4>
           <p className='text-center font-bold text-lg'>{profissional.duracao}min</p>
         </div>
+        <Rating className='mt-2 self-center' name="size-large" value={profissional.avaliacao}  precision={0.1} readOnly size="small" />
 
-        <div className='mt-7'>
+        <div className='mt-4 mb-2'>
           <h4 className='text-center font-semibold'>Consulta:</h4>
           <p className='text-center font-bold text-2xl mt-2'>{profissional.preco}</p>
+          
         </div>
+
+
+
       </div>
 
       <div className='w-[75%] '>
@@ -47,11 +55,16 @@ const Card = ({ profissional }) => {
           </div>
           <div>
             <h4 className='text-center text-lg mt-3'>Sobre mim:</h4>
-            <p className=''>Olá! Meu nome é Laysa e sou uma nutricionista apaixonada pelo meu trabalho. Eu me formei em uma universidade renomada e tenho experiência em diferentes {profissional.descricao}</p>
+            <p className=''>{descricaoProfissional.length > 175 ? (
+              <span>{descricaoProfissional.substring(0, 175) + '...'}</span>
+            ) : (
+              <span>{descricaoProfissional}</span>
+            )}
+            
+            </p>
           </div>
 
-            <button className='self-end px-3 mr-[-6%] py-1.5 rounded-2xl bg-nutri bg-opacity-80 font-medium text-white'>Saiba mais</button>
-
+          <button className=' px-3 my-3 py-1.5 rounded-2xl bg-nutri bg-opacity-80 font-medium text-white'>Saiba mais</button>
         </div>
 
       </div>
