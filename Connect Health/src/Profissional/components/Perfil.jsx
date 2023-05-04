e5912666a86334f309d22446ee7576d650614060
wendel <wh68639@gmail.com>
2023-05-04 17:55:21 -0300

    ok

--- Connect Health/src/Profissional/components/Perfil.jsx
+++ Connect Health/src/Profissional/components/Perfil.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Fundo from '../../../assets/fundo.png'
+import TituloFoto from './TituloFoto'
 
 const Perfil = () => {
   return (
     <div className='border w-4/5 m-auto h-[90vh] rounded-3xl max-h-screen'>
         <div className='h-1/5 w-full '>
             <img src={Fundo} className='h-full w-full object-cover rounded-tl-3xl rounded-tr-3xl' alt="" />
+            <TituloFoto />
         </div>
     </div>
   )
---
+++ Connect Health/src/Profissional/components/TituloFoto.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const TituloFoto = () => {
+  return (
+    <div className='flex items-center w-full'>
+        <div className='w-1/4 border-nutri border h-5'>
+            <img src="" alt="" />
+            ok
+        </div>
+
+        <div className='w-3/4 border-psi border '>
+            <h1 className='pl-10 text-3xl'>Laysa Lopes da Silva</h1>
+        </div>
+    </div>
+  )
+}
+
+export default TituloFoto
