caef7fe81b64895e057ab8acded064112dc37fe3
wendel <wh68639@gmail.com>
2023-05-04 19:52:11 -0300

    divisões de components

---
+++ Connect Health/src/Profissional/components/minComponents/Avaliacoes.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Avaliacoes = () => {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default Avaliacoes
---
+++ Connect Health/src/Profissional/components/minComponents/Especialidades.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const especialidades = () => {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default especialidades
