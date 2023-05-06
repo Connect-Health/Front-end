5c536c8f6ae23319baeeea4024b88d391e5782fc
wendel <wh68639@gmail.com>
2023-05-05 21:47:01 -0300

    ok

--- Connect Health/src/Profissional/components/minComponents/Avaliacoes.jsx
+++ Connect Health/src/Profissional/components/minComponents/Avaliacoes.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import CardAvaliacao from './CardAvaliacao'
 
 const Avaliacoes = () => {
   return (
@@ -7,6 +8,8 @@ const Avaliacoes = () => {
           <h2 className='text-2xl font-bold text-[#55DF5A]'>Avaliações mais recentes</h2>
           <p className='text-black/60 font-semibold mt-3'>Confira aqui os feddbacks dos pacientes que já passaram com Laysa </p>
         </div>
+
+        <CardAvaliacao/>
     </div>
   )
 }
---
+++ Connect Health/src/Profissional/components/minComponents/CardAvaliacao.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const CardAvaliacao = () => {
+  return (
+    <div className='bg-black/10'>
+        <div className=' ml-4 pt-4'>
+            <p className='text-white bg-[#55DF5A] w-14 h-14 font-bold rounded-full flex items-center justify-center'>5,0</p>
+        </div>
+    </div>
+  )
+}
+
+export default CardAvaliacao
