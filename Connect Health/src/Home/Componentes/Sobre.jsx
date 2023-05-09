e1960c04c0b75a6c2295666e1d1cb64538a7fa89
wendel <wh68639@gmail.com>
2023-05-08 21:24:42 -0300

    icones ok

--- Connect Health/src/Home/Componentes/Sobre.jsx
+++ Connect Health/src/Home/Componentes/Sobre.jsx
@@ -1,6 +1,6 @@
 import ModelSobre from '../../../assets/modelsobre.svg'
 import Logo from '../../../assets/logo_preta.png'
-
+import { FaHeart, FaHeartbeat } from 'react-icons/fa';
 
 
 function Sobre(){
@@ -32,23 +32,28 @@ function Sobre(){
                         <h2 className='text-xl text-center font-bold pt-6'>Plano mensal Gratuito <br/> R$00,00</h2>
                         <p>Você tem direito a:</p>
                         <div>
-                            <div>
+                            <div className='flex items-center gap-1'>
+                                <FaHeartbeat className="text-[4px] " />
                                 <p>2 consultas com Psicólogo;</p>
                             </div>
                             
-                            <div>
+                            <div className='flex items-center gap-1'>
+                                <FaHeartbeat className="text-[4px] " />
                                 <p>1 consulta com Nutricionista;</p>
                             </div>
                             
-                            <div>
+                            <div className='flex items-center gap-1'>
+                                <FaHeartbeat className="text-[4px] " />
                                 <p>Consultas de 30 minutos; </p>
                             </div>
                             
-                            <div>
+                            <div className='flex items-center gap-1'>
+                                <FaHeartbeat className="text-[4px] " />
                                 <p>Acesso ao feed de Notícias;</p>
                             </div>
 
-                            <div>
+                            <div className='flex items-center gap-1'>
+                                <FaHeartbeat className="text-[4px] " />
                                 <p>Área de gerenciamento de consultas.</p>
                             </div>
                             
