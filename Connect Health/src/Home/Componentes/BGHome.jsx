3e4d4e3c80ae94a0a6e42b90b2c99c7c1bb1dd59
willrizzo <willianrizzoratao10@gmail.com>
2023-05-17 16:48:20 -0300

    inicio responsivo

--- Connect Health/src/Home/Componentes/BGHome.jsx
+++ Connect Health/src/Home/Componentes/BGHome.jsx
@@ -24,17 +24,23 @@ function BGHome(){
     }, [])
 
     return(
-        <div className=''>
+        <div className='max-md:h-52'>
             <div className={`relative`} style={{ height: window.innerHeight}}>
                 <img className={`w-full absolute`} style={{ transform: `translateY(${offset * 0.35}px)`}} src={fundo_1} alt="" />
                 <img className='w-full absolute' style={{ transform: `translateY(${offset * 0.3}px)`}} src={cabeca_1} alt="" />
                 <img className='w-full absolute' style={{ transform: `translateY(${offset * 0.1}px)`}} src={mulher_1} alt="" />
             </div>
 
-            <div className='w-[20%] h-auto p-5 bg-[#d3d3d3] backdrop-blur-xl rounded-2xl bg-opacity-50 absolute top-[35%] left-[2.7%] z-10' style={{ transform: `translateX(${-offset * .5}px)`}}>
-                <h3 className='text-center font-bold'>Mantenha-se conectado à sua saúde!</h3>
-                <p className='text-sm text-center pt-3'>A saúde é um bem precioso que merece ser valorizado e cuidado com atenção. Aqui acreditamos que a saúde é a base para alcançar nossos objetivos e desfrutar de uma vida plena.</p>
-                <img className='w-[30%] pt-5  m-auto  ' src={Logo} alt="" />
+            <div className='w-[20%] h-auto p-5 bg-[#d3d3d3] backdrop-blur-xl rounded-2xl bg-opacity-50 absolute top-[35%] left-[2.7%] z-10
+            max-md:h-58 max-md:w-52 max-md:p-4'
+             style={{ transform: `translateX(${-offset * .5}px)`}}>
+                <h3 className='text-center font-bold
+                max-md:text-[12px]
+                '>Mantenha-se conectado à sua saúde!</h3>
+                <p className='text-sm text-center pt-3
+                max-md:text-[9px]
+                '>A saúde é um bem precioso que merece ser valorizado e cuidado com atenção. Aqui acreditamos que a saúde é a base para alcançar nossos objetivos e desfrutar de uma vida plena.</p>
+                <img className='w-[30%] pt-5  m-auto' src={Logo} alt="" />
             </div>
         </div>
     )
--- Connect Health/src/Home/Componentes/Header.jsx
+++ Connect Health/src/Home/Componentes/Header.jsx
@@ -12,14 +12,20 @@ const Header = () => {
           <Link to='/nutricao' className='text-white uppercase text-sm hover:scale-110 hover:font-semibold transition duration-300'>Nutrição</Link>
           <Link to='/' className='text-white uppercase text-sm hover:scale-110 hover:font-semibold transition duration-300'>Cadastro</Link>
         </div>
-        <div className='bg-[#0575E6] h-auto w-40 absolute z-50 top-0 right-20 items-center flex-col'>
+        <div className='bg-[#0575E6] h-auto w-40 absolute z-50 top-0 right-20 items-center flex-col
+        max-md:h-32 max-md:w-28 max-md:
+        '>
           <img src={vetor} alt=""  className='absolute'/>
           <div>
-            <h2 className='text-[#fff] text-center pt-20 pb-6 '>Tenha dicas dos melhores da área</h2>
+            <h2 className='text-[#fff] text-center pt-20 pb-6
+            max-md: text-[10px] max-md:pt-10 max-md:pb-2
+            '>Tenha dicas dos melhores da área</h2>
           </div>
             
           <div className='flex items-center justify-center gap-4 pb-4 hover:scale-110 hover:font-semibold transition duration-300'>
-              <Link to='/' className='text-[#fff] text-center font-bold text-sm '>Nosso Feed</Link>
+              <Link to='/' className='text-[#fff] text-center font-bold text-sm 
+              max-md: text-[10px]
+              '>Nosso Feed</Link>
               <BsArrowRight className='text-lg text-[#fff]'/>
           </div>  
         </div>
