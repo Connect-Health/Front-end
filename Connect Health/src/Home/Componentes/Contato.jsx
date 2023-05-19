034e4d329b1bc5b32b93d2d4a1eef9c747ac0e61
willrizzo <willianrizzoratao10@gmail.com>
2023-05-19 16:40:04 -0300

    home 90% conlcuida responsivo

--- Connect Health/src/Home/Componentes/Contato.jsx
+++ Connect Health/src/Home/Componentes/Contato.jsx
@@ -8,16 +8,18 @@ import { Link } from 'react-router-dom'
 
 function Contato(){
     return(
-        <div className='max-md:mt-[1500px]'>
+        <div className='max-md:mt-[1100px]'>
             <div className='w-[100%]'>
-                <h1 className='text-center text-3xl font-bold text-[#5855F2]'>Entre em contato conosco!</h1>
+                <h1 className='text-center text-3xl font-bold text-[#5855F2]
+                max-md:text-xl
+                '>Entre em contato conosco!</h1>
             </div>
 
             <div className='w-[90%] m-auto  mt-10 flex gap-40 items-center mb-24
-            max-md:w-[100%] max-md:justify-center max-md: bg-cor-primaria
+            max-md:w-[95%] max-md:justify-cente max-md:flex-col max-md:gap-14
             '>
                 <div className=' flex flex-col h-auto pb-4 bg-[#5855F2] bg-opacity-80 w-[70%] pl-10 pt-10 rounded-2xl shadow-lg
-                max-md:w-[100%] 
+                max-md:w-[100%]
                 '>
                     <div>
                         <h2 className='font-bold text-2xl text-white
@@ -31,23 +33,25 @@ function Contato(){
                     <div className='flex flex-col gap-10 mt-10
                     max-md:w-[100%]
                     '>
-                        <input placeholder='Nome' className='w-[70%] h-[10%] bg-gradi bg-opacity-0 border-b-2 border-white placeholder:text-white text-white focus:outline-none' type="text"/>
-                        <input placeholder='Email' className='w-[70%] h-[10%] bg-gradi bg-opacity-0 border-b-2 border-white placeholder:text-white text-white focus:outline-none' type="email" maxLength={11}/>
+                        <input placeholder='Nome' className='w-[70%] h-[10%] bg-gradi bg-opacity-0 border-b-2 border-white placeholder:text-white text-white focus:outline-none
+                        max-md:w-[80%]
+                        ' type="text"/>
+                        <input placeholder='Email' className='w-[70%] h-[10%] bg-gradi bg-opacity-0 border-b-2 border-white placeholder:text-white text-white focus:outline-none
+                        max-md:w-[80%]
+                        ' type="email" maxLength={11}/>
                         <input placeholder='Telefone (55)' className='w-[70%] h-[10%] bg-gradi bg-opacity-0 border-b-2 border-white placeholder:text-white text-white placeholder:shadow-none focus:outline-none
-                       
+                        max-md:w-[80%]
                         ' type="tel" maxLength={11}/>
                         <textarea placeholder='Mensagem' maxLength={255} className='bg-[#F3F9FF] py-3 px-4 opacity-80 w-[70%] h-40 rounded-sm placeholder:text-black  text-black text
-                         max-md:w-[100%]
+                         max-md:w-[80%]
                         ' name="" id="" cols="40" rows="10"></textarea>
-                        <button className='bg-[#5855F2] text-white shadow-xl w-[35%] rounded-sm p-2 hover:bg-white hover:text-[#5855F2] transition-all duration-300
-                        max-md:ml-12
-                        '>Enviar</button>
+                        <button className='bg-[#5855F2] text-white shadow-xl w-[35%] rounded-sm p-2 hover:bg-white hover:text-[#5855F2] transition-all duration-300'>Enviar</button>
                     </div>
                 </div>
 
                 <div>
                     <img className='w-[100%]
-                    max-md:hidden
+                    
                     ' src={DesenhoContato} alt="" />
                 </div>
             </div>
