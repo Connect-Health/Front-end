25252bdd5ed84bd3d7f9f2a13f7c2932421e487e
wendel <wh68639@gmail.com>
2023-05-25 20:33:32 -0300

    agora finalizado

--- Connect Health/src/Complementares/Politica/Politica.jsx
+++ Connect Health/src/Complementares/Politica/Politica.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Header from '../../Profissional/components/Header'
 import Footer from '../../Components/Footer'
 import Termos from './components/Termos'
 
 const Politica = () => {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
   return (
     <div>
         <Header 
