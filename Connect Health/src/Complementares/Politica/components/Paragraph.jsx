3a47946cafc63e0c6b40a1eddd1b094a7340b8b7
wendel <wh68639@gmail.com>
2023-05-25 23:56:22 -0300

    ok

--- Connect Health/src/Complementares/Politica/components/Paragraph.jsx
+++ Connect Health/src/Complementares/Politica/components/Paragraph.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Paragraph = (props) => {
   return (
     <div className='mt-2'>
-        <p className='ml-5'> {props.text}</p>
+        <p className='ml-5 max-md:ml-2 max-md:text-sm'> {props.text}</p>
     </div>
   )
 }
