c190f53e5147a5d2683c75467eca0030af980565
Gabriela Nunes <92945219+GabyNunes17@users.noreply.github.com>
2023-05-26 19:53:40 -0300

    login falhou, mas vou resolver paee

--- Connect Health/src/Components/Login/Login.jsx
+++ Connect Health/src/Components/Login/Login.jsx
@@ -9,6 +9,13 @@ import GoogleLogin from 'react-google-login'
 
 function Login (){
 
+   const onSuccess = (res) => {
+      console.log("LOGIN EFETUADO COM SUCESSO")
+   }
+
+    const onFailure = (res) => {
+      console.log("LOGIN FALHOU COM SUCESSO")
+    }
    const responseGoogle = (response) => {
       console.log(response);
     }
@@ -43,10 +50,10 @@ function Login (){
                          </button>
                                  <div className='flex'>
                                     <GoogleLogin
-                                       clientId="471911563497-1ja3qk8l95t2sjlo761kd2bmohnruecu.apps.googleusercontent.com"
-                                       buttonText=" né nada"
-                                       onSuccess={responseGoogle}
-                                       onFailure={responseGoogle}
+                                       clientId="1030844756804-vjfa1ifrgc703mc5cfa0iqk6tl19qaph.apps.googleusercontent.com"
+                                       buttonText=""
+                                       onSuccess={onSuccess}
+                                       onFailure={onFailure}
                                     />
                                     <Link to='/'><RiFacebookCircleFill className='text-azulsite text-5xl' /></Link>
                                     <Link to='/'> <RiLinkedinFill className='text-azulsite text-5xl pl-3' /></Link>
