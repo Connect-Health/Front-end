f2b806d51cd2b292f6407a71f8764e71ebc03093
wendel <wh68639@gmail.com>
2023-05-26 13:51:11 -0300

    teste menu

---
+++ Connect Health/src/Components/Menu.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react'
+import BarsSolid from '../../assets/bars-solid.svg'
+import { AiOutlineClose } from 'react-icons/ai'
+
+
+const Menu = () => {
+    const [open, setOpen] = useState(false)
+
+    const abrirMenu = () => {
+        setOpen(!open);
+    }
+  return (
+    <div className='max-md:block'>
+        <button onClick={abrirMenu} className='h-5 w-5'>
+                <img src={BarsSolid} alt="" />
+        </button>
+
+        {open && (
+            <div className="fixed inset-0 bg-white z-50">
+            <div className="max-w-md mx-auto py-4 px-6">
+                <AiOutlineClose className='absolute top-3 right-3' />
+              <ul>
+                <li>Item 1</li>
+                <li>Item 2</li>
+                <li>Item 3</li>
+              </ul>
+            </div>
+          </div>
+        )}
+    </div>
+  )
+}
+
+export default Menu
