b909404dee1bcee0722235f13e0f273b7910fb3c
Rafael Ferreira Machado <rafaelf.machado2004@gmail.com>
2023-05-27 21:46:03 -0300

    Login com google

--- Connect Health/src/Components/Login/Login.jsx
+++ Connect Health/src/Components/Login/Login.jsx
@@ -1,81 +1,100 @@
+import { AiOutlineArrowLeft } from "react-icons/ai";
+import { Link } from "react-router-dom";
+import { RiFacebookCircleFill, RiLinkedinFill } from "react-icons/ri";
+import lockericon from "../../../assets/lockericon.png";
+import mailicon from "../../../assets/mailicon.png";
+import BgLogin from "../../../assets/bglogin.png";
+import { FcGoogle } from "react-icons/fc";
 
-import {AiOutlineArrowLeft} from 'react-icons/ai'
-import { Link } from 'react-router-dom'
-import { RiFacebookCircleFill, RiLinkedinFill } from 'react-icons/ri'
-import lockericon from '../../../assets/lockericon.png'
-import mailicon from '../../../assets/mailicon.png'
-import BgLogin from "../../../assets/bglogin.png"
-import {FcGoogle} from 'react-icons/fc'
-import axios from 'axios';
+import GoogleLogin from "react-google-login";
+import { gapi } from "gapi-script";
+import { useEffect } from "react";
 
-import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
-import { app } from './firebase'
+function Login() {
 
-const provider = new GoogleAuthProvider();
+  useEffect(() => {
+    gapi.load("auth2", () => {
+      gapi.auth2.init({client_id: "1030844756804-m94rijfndnkioau7eph93tf51rmcukqk.apps.googleusercontent.com"});
+    });
+  }, []);
 
+  const responseGoogle = (response) => {
+    console.log(response);
+  };
 
-
-function Login (){
-   const auth = getAuth(app);
-
-   const singInGoogle = () => {
-      
-      signInWithPopup(auth, provider)
-        .then((result) => {
-          const credential = GoogleAuthProvider.credentialFromResult(result);
-          const token = credential.accessToken;
-          const user = result.user;
-          console.log(user);
-         
-        }).catch((error) => {
-          const errorCode = error.code;
-          const errorMessage = error.message;
-          const email = error.customData.email;
-          const credential = GoogleAuthProvider.credentialFromError(error);
-   
-        });
-      
-      }
-     
-
-    return (
-        <div className='flex'>
-              <div className=" flex w-3/5 h-screen  bg-cover bg-center items-center">
-                 <img src={BgLogin} alt="" className='absolute -z-10 w-3/5' />
-                 <Link to='/'>
-                 <AiOutlineArrowLeft  className='absolute text-white text-5xl top-3 left-3'/>
-                 </Link>
-                    <div className="flex flex-col ml-20">
-                       <h1 className="font-bold text-4xl text-white">Connect Health</h1>
-                       <h2 className="text-2xl text-white mt-2">Mantenha-se conectado com sua saúde!</h2>
-                    </div>
-             </div>
-             <div className='flex flex-col items-center w-[40%] justify-center gap-6'>
-                 <h1 className='font-extrabold text-4xl'>Bem Vindo(a)!</h1>
-                   <div className='flex mr-5'>
-                       <img className='relative left-10 top-2 w-6 h-6' src={mailicon} alt="icone do email" />
-                       <input type='text' className='border-solid border-2 border-gradi/30 rounded-3xl w-80 h-10 pl-11 required '  placeholder='E-mail'/>
-                   </div>
-                      <div className='flex mr-5'>
-                         <img className='relative left-10 top-2 w-6 h-6' src={lockericon} alt="icone do email" />
-                         <input type='password' className='border-solid border-2 border-gradi/30 rounded-3xl w-80 h-10 pl-11 required '  placeholder='Senha'/>
-                      </div>
-                         <button type='submit' className= ' flex items-center justify-center bg-azulsite border-solid border-2 border-gradi/30 rounded-3xl w-80 h-12'>
-                           <h1 className='text-white text-1xl'>Entrar</h1>
-                         </button>
-                         <button type='submit' className= 'text-gradi/70'>
-                           <h1 className='text-gradi/80 text-1x1 font-bold'>Esqueci a senha</h1>
-                         </button>
-                                 <div className='flex'>
-                                    <FcGoogle className='text-5xl pl-3' onClick={singInGoogle}/>
-                                    <Link to='/'><RiFacebookCircleFill className='text-azulsite text-5xl' /></Link>
-                                    <Link to='/'> <RiLinkedinFill className='text-azulsite text-5xl pl-3' /></Link>
-                                 </div>
-                                 <h1 className='text-1xl font-semibold mt-3'>Não tem conta? <Link to='/register' className='text-azulsite'>Registre-se</Link></h1>
-                         
-             </div>
-     </div>
-
-    )
+  return (
+    <div className="flex">
+      <div className=" flex w-3/5 h-screen  bg-cover bg-center items-center">
+        <img src={BgLogin} alt="" className="absolute -z-10 w-3/5" />
+        <Link to="/">
+          <AiOutlineArrowLeft className="absolute text-white text-5xl top-3 left-3" />
+        </Link>
+        <div className="flex flex-col ml-20">
+          <h1 className="font-bold text-4xl text-white">Connect Health</h1>
+          <h2 className="text-2xl text-white mt-2">
+            Mantenha-se conectado com sua saúde!
+          </h2>
+        </div>
+      </div>
+      <div className="flex flex-col items-center w-[40%] justify-center gap-6">
+        <h1 className="font-extrabold text-4xl">Bem Vindo(a)!</h1>
+        <div className="flex mr-5">
+          <img
+            className="relative left-10 top-2 w-6 h-6"
+            src={mailicon}
+            alt="icone do email"
+          />
+          <input
+            type="text"
+            className="border-solid border-2 border-gradi/30 rounded-3xl w-80 h-10 pl-11 required "
+            placeholder="E-mail"
+          />
+        </div>
+        <div className="flex mr-5">
+          <img
+            className="relative left-10 top-2 w-6 h-6"
+            src={lockericon}
+            alt="icone do email"
+          />
+          <input
+            type="password"
+            className="border-solid border-2 border-gradi/30 rounded-3xl w-80 h-10 pl-11 required "
+            placeholder="Senha"
+          />
+        </div>
+        <button
+          type="submit"
+          className=" flex items-center justify-center bg-azulsite border-solid border-2 border-gradi/30 rounded-3xl w-80 h-12"
+        >
+          <h1 className="text-white text-1xl">Entrar</h1>
+        </button>
+        <button type="submit" className="text-gradi/70">
+          <h1 className="text-gradi/80 text-1x1 font-bold">Esqueci a senha</h1>
+        </button>
+        <div className="flex">
+          <FcGoogle className="text-5xl pl-3" />
+          <GoogleLogin
+            clientId="1030844756804-m94rijfndnkioau7eph93tf51rmcukqk.apps.googleusercontent.com"
+            textButton=""
+            onSuccess={responseGoogle}
+            onFailure={responseGoogle}
+            />
+          <Link to="/">
+            <RiFacebookCircleFill className="text-azulsite text-5xl" />
+          </Link>
+          <Link to="/">
+            {" "}
+            <RiLinkedinFill className="text-azulsite text-5xl pl-3" />
+          </Link>
+        </div>
+        <h1 className="text-1xl font-semibold mt-3">
+          Não tem conta?{" "}
+          <Link to="/register" className="text-azulsite">
+            Registre-se
+          </Link>
+        </h1>
+      </div>
+    </div>
+  );
 }
-export default Login
+export default Login;
