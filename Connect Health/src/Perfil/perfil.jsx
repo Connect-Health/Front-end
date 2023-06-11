9eeb5be1a10d5b86a9dca02406c9782553a3dbed
willrizzo <willianrizzoratao10@gmail.com>
2023-06-11 18:49:48 -0300

    so colocar api

--- Connect Health/src/Perfil/perfil.jsx
+++ Connect Health/src/Perfil/perfil.jsx
@@ -4,8 +4,9 @@ import ProfilePic from '../../assets/profilepic.png'
 import { AiFillAppstore } from "react-icons/ai";
 import settings from '../../assets/setting.png'
 import Dashboard from './Components/Dashboard';
-import {AiFillBell} from 'react-icons/ai'
-
+import { AiFillBell } from 'react-icons/ai'
+import {AiOutlineMore} from 'react-icons/ai'
+import {AiOutlineSound} from 'react-icons/ai'
 
 
 function Perfil() {
@@ -21,7 +22,7 @@ function Perfil() {
                     <div className='flex flex-col pl-8 gap-[55px] w-[100%] '>
 
                         <div className='flex gap-4'>
-                            
+
                             <p className=''>Painel de controle</p>
                         </div>
                         <div className='flex gap-4'>
@@ -37,7 +38,7 @@ function Perfil() {
 
                     <div className='flex flex-col pl-8 gap-[50px] w-[100%] '>
                         <div className='flex gap-4'>
-                            
+
                             <p className=''>Configurações</p>
                         </div>
                         <div className='flex gap-4'>
@@ -52,16 +53,42 @@ function Perfil() {
                 <Dashboard />
                 <div className="bg-[#9DFF95] w-[26%] h-[94vh] rounded-lg mt-[2%] shadow-2xl flex justify-center float-left -ml-44">
                     <div className='flex flex-col items-center gap-5 w-[80%]'>
-                        <img className='object-cover rounded-full w-[200px] h-[200px] mt-10 border-4 border-[#38dd29]' src={ProfilePic} alt="" />
-                        <div className='flex flex-col gap-3'>
+                        <img className='object-cover rounded-full w-[180px] h-[180px] mt-10 border-4 border-[#38dd29]' src={ProfilePic} alt="" />
+                        <div className='flex flex-col gap-2'>
                             <h1 className='text-center font-bold text-xl'>Willian Rizzo</h1>
-                            <p  className='text-center text-lg'>Designer UI | UX </p>
+                            <p className='text-center text-base'>Designer UI | UX </p>
                         </div>
-                        <div className=' flex flex-col w-[100%] mt-6'>
+                        <div className=' flex flex-col w-[100%] mt-2'>
                             <div className=' flex items-center justify-between'>
                                 <h2 className='text-xl font-bold'>Alarmes</h2>
                                 <div className='bg-white p-2 rounded-full '>
-                                    <AiFillBell className='text-3xl text-[#ddd018] '/>
+                                    <AiFillBell className='text-3xl text-[#ddd018] ' />
+                                </div>
+                            </div>
+                        </div>
+                        <div className='w-[100%] mt-3 flex flex-col gap-6'>
+                            <div className='bg-[#f5f5f5] rounded-md h-[10vh] flex items-center justify-between p-2 '>
+                                <div className='bg-[#ffd043a0] rounded-lg p-2 ml-'>
+                                    <AiOutlineSound className='text-[#FF993A] text-3xl'/>
+                                </div>
+                                <div className='w-[100%] ml-8'>
+                                    <p className='text-[12px] font-bold '>Próxima Consulta</p>
+                                    <p className='text-[12px]'>8:00 - 12:00</p>
+                                </div>
+                                <div>
+                                    <AiOutlineMore className='text-2xl'/>
+                                </div>
+                            </div>
+                            <div className='bg-[#f5f5f5] rounded-md h-[10vh] flex items-center justify-between p-2 '>
+                                <div className='bg-[#ffd043a0] rounded-lg p-2 ml-'>
+                                    <AiOutlineSound className='text-[#FF993A] text-3xl'/>
+                                </div>
+                                <div className='w-[100%] ml-8'>
+                                    <p className='text-[12px] font-bold '>Próxima Consulta</p>
+                                    <p className='text-[12px]'>8:00 - 12:00</p>
+                                </div>
+                                <div>
+                                    <AiOutlineMore className='text-2xl'/>
                                 </div>
                             </div>
                         </div>
