0980c9f66d1aa828d0b7c0bc1917b9165d7018e6
Guilherme Gabriel Garcia <125367739+GuilhermeGGarcia12@users.noreply.github.com>
2023-06-16 20:16:55 -0300

    parte do pagamento feito

--- Connect Health/src/Complementares/Pagamentos/Pagamentos.jsx
+++ Connect Health/src/Complementares/Pagamentos/Pagamentos.jsx
@@ -1,18 +1,40 @@
 import React from 'react'
 import Header from '../../Components/Header'
 
+import Menu from '../../Components/Menu'
+
+import Ajuda from '../../../assets/ajuda.jpg'
+import seguro from '../../../assets/seguro.png'
+
 function Pagamentos () {
     return (
         <div className='bg-[#f1f1dc]'>
-            <Header
+        <Header
             health="hover:text-azulsite"
             connect="text-azulsite"
             area="Pagamento"
             colorArea="hover:text-[#0f44ff]"
             link="/"
             areaNav="Inicio"
+            colorAreaNav="Inicio hover:text-[#0f44ff]"
             pai="relative z-10"
-            />
+        />
+        <Menu
+            area1="Inicio"
+            area2="Area psicologia"
+            area3="Area nutrição"
+            area4="Feed de noticias"
+            
+            home=""
+            link2="psicologia"
+            link3="nutricao"
+            link4="feed"
+        />
+
+        <img className='absolute top-0 z-0 w-full h-[25rem] object-cover max-md:h-[18rem]' src={Ajuda} />
+
+        <img className='m-auto w-14 mt-[24rem]' src={seguro} />
+
         </div>
     )
 }
