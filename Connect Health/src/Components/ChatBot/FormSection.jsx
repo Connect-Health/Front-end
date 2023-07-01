ccf4205d5e5a485a883c4d5a3ce56fede76de17d
Gabriela Nunes <92945219+GabyNunes17@users.noreply.github.com>
2023-07-01 15:05:06 -0300

    perguntas adicionadas

--- Connect Health/src/Components/ChatBot/FormSection.jsx
+++ Connect Health/src/Components/ChatBot/FormSection.jsx
@@ -1,6 +1,5 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { AiOutlineSend } from "react-icons/ai";
-import { Microfone } from "./Microfone";
 
 const FormSection = ({ generateResponse }) => {
   const [newQuestion, setNewQuestion] = useState("");
@@ -8,26 +7,28 @@ const FormSection = ({ generateResponse }) => {
   const handleKeyPress = (e) => {
     if (e.key === "Enter") {
       generateResponse(newQuestion, setNewQuestion);
+      setNewQuestion(""); // Limpar a caixa de texto
     }
   };
 
   return (
-    <div className=" flex items-center gap-2 w-full absolute bottom-0 ">
-      <div className="w-14 ">
-
-      </div>
+    <div className="flex items-center gap-2 w-full absolute bottom-0">
+      <div className="w-14"></div>
 
       <input
         type="text"
-        className="form-control bg-[#47ace7]  py-2 pl-5 w-[95%] rounded-lg text-white placeholder:text-white outline-none"
+        className="form-control bg-[#47ace7] py-2 pl-5 w-[95%] rounded-lg text-white placeholder:text-white outline-none"
         placeholder="Me pergunte algo..."
         value={newQuestion}
         onChange={(e) => setNewQuestion(e.target.value)}
         onKeyDown={handleKeyPress}
       />
       <button
-        className=" bg-[#13d6dd] p-2 rounded text-2xl "
-        onClick={() => generateResponse(newQuestion, setNewQuestion)}
+        className="bg-[#13d6dd] p-2 rounded text-2xl"
+        onClick={() => {
+          generateResponse(newQuestion, setNewQuestion);
+          setNewQuestion(""); // Limpar a caixa de texto
+        }}
       >
         <AiOutlineSend />
       </button>
