f463dd04e232ce54eb62277257bcdd79ad7d6586
Rafael Ferreira Machado <rafaelf.machado2004@gmail.com>
2024-04-23 22:24:37 -0300

    fix: update text for psychology consultancy services

--- Connect Health/src/Components/Detalhes.jsx
+++ Connect Health/src/Components/Detalhes.jsx
@@ -8,17 +8,17 @@ const Detalhes = (props) => {
 
         <div className='text-center text-white mt-5 mb-5 w-1/3 ml-5 max-md:w-1/2 '>
             <h2 className='text-2xl font-semibold drop-shadow-xl mb-4 '>Nossos Planos</h2>
-            <p className='text-lg drop-shadow-md max-md:text-sm'>Nós oferecemos planos mensais e consultas avulsas a preços acessíveis, para tornar nossos serviços de consultoria em nutrição acessíveis a todos os interessados em investir em sua saúde e bem-estar.</p>
+            <p className='text-lg drop-shadow-md max-md:text-sm'>Nós oferecemos planos mensais e consultas avulsas a preços acessíveis, para tornar nossos serviços de consultoria em psicologia acessíveis a todos os interessados em investir em sua saúde e bem-estar.</p>
         </div>
 
         <div className='text-center text-white mt-5 mb-5 w-1/3 border-x-2  px-14 box-content '>
             <h2 className='text-2xl font-semibold drop-shadow-xl mb-4 '>Nossos Profissionais</h2>
-            <p className='text-lg drop-shadow-md'>Contamos com uma equipe de profissionais altamente qualificados, formados nas melhores faculdades e universidades, prontos para fornecer serviços de consultoria em nutrição de excelência</p>
+            <p className='text-lg drop-shadow-md'>Contamos com uma equipe de profissionais altamente qualificados, formados nas melhores faculdades e universidades, prontos para fornecer serviços de consultoria em psicologia de excelência</p>
         </div>
 
         <div className='text-center text-white mt-5 mb-5 w-1/3 mr-10'>
             <h2 className='text-2xl font-semibold drop-shadow-xl mb-4 '>Consultas On-line</h2>
-            <p className='text-lg drop-shadow-md'>Aproveite a comodidade e praticidade de nossos serviços de consultoria em nutrição online, sem a necessidade de sair de casa ou enfrentar longas filas e processos burocráticos.</p>
+            <p className='text-lg drop-shadow-md'>Aproveite a comodidade e praticidade de nossos serviços de consultoria em psicologia online, sem a necessidade de sair de casa ou enfrentar longas filas e processos burocráticos.</p>
         </div>
     </div>
   )
