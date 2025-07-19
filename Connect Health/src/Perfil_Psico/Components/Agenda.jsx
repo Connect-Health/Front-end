8b645d15143b6c027a0cd2cdaae1037618376b38
GABII <you@example.com>
2025-07-18 22:32:03 -0300

    feat: agenda pt2

---
+++ Connect Health/src/Perfil_Psico/Components/Agenda.jsx
@@ -0,0 +1,12 @@
+
+function Agenda() {
+    
+    return (
+
+        <div className="bg-white h-[94%] w-[85%] rounded-lg mt-[2%] shadow-2xl">
+            <h1>Agenda</h1>
+        </div>
+    )
+}
+export default Agenda;  
+
